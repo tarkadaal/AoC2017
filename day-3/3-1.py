17b39639af95106871aed34d34f673254b35c05d
Dan Jackson <danjacksonis@googlemail.com>
2017-12-05 00:01:32 +0100

    Tidied it up a bit

--- day-3/3-1.py
+++ day-3/3-1.py
@@ -23,46 +23,42 @@ START = (500, 500)
 def manhattan_distance(start, end):
     return abs(end[0] - start[0]) + abs(end[1] - start[1])
 
-def sum_neighbours(val, pos, board):
+def sum_neighbours(pos, board):
     return sum(map(lambda x: board[pos[0] + x[0]][pos[1] + x[1]], ALL_DIRECTIONS)) + board[pos[0]][pos[1]]
-
-def val(val, pos, board):
-    return val
-
 
 def get_cell_to_left(pos, dir, board):
     left = WIDDERSHINS[dir]
     newpos = pos[0] + left[0], pos[1] + left[1]
     return board[newpos[0]][newpos[1]]
 
-def _solve(target, pos, dir, board, func):
+def move_to_next(this_cell, pos, dir, board):
+    board[pos[0]][pos[1]] = this_cell
+    if get_cell_to_left(pos, dir, board) == 0:
+        dir = WIDDERSHINS[dir]
+    pos = pos[0] + dir[0], pos[1] + dir[1]
+    return pos, dir, board
+
+def _solve(target, pos, dir, board):
     for x in xrange(1, target + 1):
+        this_cell = x
         if target == x:
             return manhattan_distance(START, pos)
-        board[pos[0]][pos[1]] = func(x, pos, board)
-        if get_cell_to_left(pos, dir, board) == 0:
-            dir = WIDDERSHINS[dir]
-        pos = pos[0] + dir[0], pos[1] + dir[1]
+        pos, dir, board = move_to_next(this_cell, pos, dir, board)
 
-
-def _solve2(target, pos, dir, board, func):
+def _solve2(target, pos, dir, board):
     for x in xrange(1, 10000000 + 1):
-        this_cell = func(x, pos, board)
+        this_cell = sum_neighbours(pos, board)
         if target < this_cell:
             return this_cell
-        board[pos[0]][pos[1]] = this_cell
-        if get_cell_to_left(pos, dir, board) == 0:
-            dir = WIDDERSHINS[dir]
-        pos = pos[0] + dir[0], pos[1] + dir[1]
-
+        pos, dir, board = move_to_next(this_cell, pos, dir, board)
 
 def solve(n):
-    return _solve(n, START, DOWN, [[0] * 1000 for i in range(1000)], val)
+    return _solve(n, START, DOWN, [[0] * 1000 for i in range(1000)])
 
 def solve2(n):
     board = [[0] * 1000 for i in range(1000)]
     board[START[0]][START[1]] = 1
-    return _solve2(n, START, DOWN, board, sum_neighbours)
+    return _solve2(n, START, DOWN, board)
 
 def test_solve(n):
     """
@@ -89,6 +85,7 @@ def test_solve2(n):
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+    
     print(solve(312051))
     print(solve2(312051))
     
