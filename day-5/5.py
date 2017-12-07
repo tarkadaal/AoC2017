cef4c45d5c65bc0e04916f034a62f13b19ccafed
Dan Jackson <danjacksonis@googlemail.com>
2017-12-07 10:13:21 +0100

    Day 5 pt 2

--- day-5/5.py
+++ day-5/5.py
@@ -10,6 +10,16 @@ def maze_escape(maze):
         current_index += instruction
     return step_total
 
+def maze_escape_2(maze):
+    step_total = 0
+    current_index = 0
+    while -1 < current_index < len(maze):
+        step_total += 1
+        instruction = maze[current_index]
+        maze[current_index] += 1 if instruction < 3 else -1
+        current_index += instruction
+    return step_total
+
 def test_maze_escape():
     """
     >>> maze_escape([0, 3, 0, 1, -3])
@@ -17,6 +27,12 @@ def test_maze_escape():
     """
     pass
 
+def test_maze_escape_2():
+    """
+    >>> maze_escape_2([0, 3, 0, 1, -3])
+    10
+    """
+    pass
 
 if __name__ == '__main__':
     import doctest
@@ -26,3 +42,4 @@ if __name__ == '__main__':
         input_data = f.readlines()
 
     print(maze_escape([int(x.strip()) for x in input_data])) # answer: 342669
+    print(maze_escape_2([int(x.strip()) for x in input_data])) # answer: 25136209
